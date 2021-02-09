0068acbbed9d76e69bac347866438b8b0826264b
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2021-02-09 13:20:12 -0700

    adds styling for the app

--- github/src/App.js
+++ github/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userData: [],
+    followerData: [],
+    buttonClicked: false
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/joshuasamaniego')
+    .then(res => {
+      this.setState({
+        userData: res.data,
+      })
+    })
+    .catch(err => {
+      console.log("Error with axios call", err);
+    })
+    axios.get('https://api.github.com/users/joshuasamaniego/followers')
+    .then(res => {
+      this.setState({
+        followerData: res.data
+      })
+    })
+    .catch(err => {
+      console.log("Error with axios call", err);
+    })
+  }
+
+  handleClick = (e) => {
+    console.log(e.target);
+    this.setState({
+      buttonClicked: true
+    })
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.buttonClicked !== prevState.buttonClicked) {
+  //     console.log("component has re-rendered, buttonClicked state has been updated");
+  //   }
+  // }
+
+  render() {
+    console.log(this.state.followerData);
+    return (
+      <div className="App">
+        <div className="user-card-container">
+          <h1>Github User Card App</h1>
+          <img className="user-img" src={this.state.userData.avatar_url} alt="profile"/>
+          <h3>{this.state.userData.name}</h3>
+          <p>{this.state.userData.bio}</p>
+          <a href={this.state.userData.url}>Follow Me On GitHub!</a>
+          <button onClick={this.handleClick}>See My Followers</button>
+        </div>
+        <div className="followers-container">
+          {this.state.buttonClicked 
+          ? 
+            this.state.followerData.map(follower => {
+              return(
+                <div className="follower-card">
+                  <img src={follower.avatar_url} alt="follower-profile"/>
+                  <h5>{follower.login}</h5>
+                </div>
+              )
+            }) 
+          : null}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+
